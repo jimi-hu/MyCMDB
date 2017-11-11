1772e34b030cc4719de8a1566a28ad58e422c0da
hujie <hujie>
2017-11-11 10:59:56 +0800

    增加model_ID

--- install/urls.py
+++ install/urls.py
@@ -9,7 +9,7 @@ from install import  views
  
 urlpatterns = [
     url(r'^index/',views.index),
-    url(r'^upload/',views.upload),
+    url(r'^upload_molde/',views.upload_model),
     url(r'^download/',views.download),
     url(r'^(?P<model_id>\d*)',views.model),
 ]
--- install/views.py
+++ install/views.py
@@ -18,7 +18,7 @@ def index(request):
     #playbook_list.path=playbook_list.path.split("/")[-1]
     return render_to_response("install.html",{"playbook_list":playbook_list})
 
-def upload(request):
+def upload_model(request):
     if request.method == "POST":
         playbook=request.FILES.get('playbook')
         dir_name=playbook.name.split(".")[0]
@@ -41,19 +41,17 @@ def upload(request):
     return render_to_response("install.html")
 
 def model(request,model_id):
-    a=job_models.objects.get(id=5)
+    a=job_models.objects.get(id=model_id)
     file=os.path.join(a.path,"group_vars/all")
     f=open(file)
     var_all_file_contex="".join(f.readlines())
-    return render_to_response("model_job.html",{"var_all_file_contex":var_all_file_contex})
+    f.close()
+    host_file=os.path.join(a.path,"hosts")
+    c=open(host_file)
+    host_file_contex="".join(c.readlines())
+    return render_to_response("model_job.html",{"var_all_file_contex":var_all_file_contex,"host_file_contex":host_file_contex})
 
 def download(request):
     if request.method == "GET":
         data=readfile.file_content_all(PROJECT_DIR+"/ansible/1/1.py")
         return HttpResponse(data)    
-        
-        
-        
-        
-                
-        
